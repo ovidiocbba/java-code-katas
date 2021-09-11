344b70d051cac726cc2d2fba153b6510ae101cb3
Ovidio Miranda <ovidiocbba@hotmail.com>
2021-09-11 16:22:53 -0400

    Add the Messages enum class

---
+++ src/main/java/com/ovidiomiranda/javacodekatas/utils/Messages.java
@@ -0,0 +1,21 @@
+package com.ovidiomiranda.javacodekatas.utils;
+
+/**
+ * This class contains all the strings that will be used in Error messages.
+ *
+ * @author Ovidio Miranda
+ */
+public final class Messages {
+
+  /**
+   * Message : "The Expect value is:{expect} but the Actual value is :{actual}".
+   */
+  public static final String NOT_EQUALS =
+      "The Expect value is: '%s' but the Actual value is : '%s'";
+
+  /**
+   * The constructor of Messages class.
+   */
+  private Messages() {
+  }
+}
