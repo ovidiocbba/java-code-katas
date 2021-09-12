a1e3a4ca8120562f75f9f995a75ea2ebff8ade84
Ovidio Miranda <ovidiocbba@hotmail.com>
2021-09-11 20:48:30 -0400

    Add the Highest and Lowest tests

---
+++ src/test/java/com/ovidiomiranda/javacodekatas/sevenkyu/HighestAndLowestTest.java
@@ -0,0 +1,36 @@
+package com.ovidiomiranda.javacodekatas.sevenkyu;
+
+import static com.ovidiomiranda.javacodekatas.utils.Messages.NOT_EQUALS;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The Class HighestAndLowestTest.
+ */
+public class HighestAndLowestTest {
+
+  /**
+   * The HighestAndLowestTest is declared.
+   */
+  private HighestAndLowest highestAndLowest;
+
+  /**
+   * This method initialize the HighestAndLowestTest class.
+   */
+  @Before
+  public void init() {
+    highestAndLowest = new HighestAndLowest();
+  }
+
+  /**
+   * Test 1.
+   */
+  @Test
+  public void test1() {
+    final String expected = "42 -9";
+    final String actual = highestAndLowest.highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4");
+    assertEquals(NOT_EQUALS, expected, actual);
+  }
+}
