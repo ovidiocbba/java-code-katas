e5353e52d6f0ea1521abb3ff4282d6b2e6eecb3f
Ovidio Miranda <ovidiocbba@hotmail.com>
2024-06-26 17:28:43 -0400

    Add Remove the minimum kata

--- src/main/java/com/ovidiomiranda/javacodekatas/sevenkyu/RemoveTheMinimum.java
+++ src/main/java/com/ovidiomiranda/javacodekatas/sevenkyu/RemoveTheMinimum.java
@@ -1,9 +1,10 @@
 package com.ovidiomiranda.javacodekatas.sevenkyu;
 
+import static java.util.stream.IntStream.range;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.IntStream;
 
 public class RemoveTheMinimum {
 
@@ -22,13 +23,14 @@ public class RemoveTheMinimum {
   }
 
   public static int[] removeSmallestV2(int[] numbers) {
-    int min = Arrays.stream(numbers).min().orElse(0);
-    return IntStream.range(0, numbers.length).filter(i -> i != min).map(i -> numbers[i]).toArray();
+    int min = range(0, numbers.length).reduce((i, j) -> numbers[i] > numbers[j] ? j : i).orElse(-1);
+    return range(0, numbers.length).filter(i -> i != min).map(i -> numbers[i]).toArray();
   }
 
   public static void main(String[] args) {
-    System.out.println(Arrays.toString(removeSmallest(new int[]{1, 2, 3, 4, 5}))); // [2,3,4,5]
-    System.out.println(Arrays.toString(removeSmallest(new int[]{5, 3, 2, 1, 4}))); // [5,3,2,4]
-    System.out.println(Arrays.toString(removeSmallest(new int[]{2, 2, 1, 2, 1}))); // [2,2,2,1]
+    System.out.println(Arrays.toString(removeSmallest(new int[]{1, 2, 3, 4, 5}))); // [2, 3, 4, 5]
+    System.out.println(Arrays.toString(removeSmallest(new int[]{5, 3, 2, 1, 4}))); // [5, 3, 2, 4]
+    System.out.println(Arrays.toString(removeSmallest(new int[]{2, 2, 1, 2, 1}))); // [2, 2, 2, 1]
+    System.out.println(Arrays.toString(removeSmallest(new int[]{1, 2, 3, 4, 5}))); // [2, 3, 4, 5]
   }
 }
